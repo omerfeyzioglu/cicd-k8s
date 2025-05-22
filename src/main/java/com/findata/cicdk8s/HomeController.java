92bcf8369fa62fa3dfa27283638e5155b2410eb5
Ömer Feyzioğlu <oomerfeyzioglu@gmail.com>
2025-05-22 10:31:50 +0300

    test for class

--- src/main/java/com/findata/cicdk8s/HomeController.java
+++ src/main/java/com/findata/cicdk8s/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Hello World!!!");
-        return "home"; 
+        model.addAttribute("message", "Hello sir!!!");
+        return "home";
     }
 }
